e3564ba2780bc4c68ad41cc27fcf6a4bdd30280d
Tien Young <tyang2896@conestogac.on.ca>
2024-06-29 21:31:44 -0400

    Update the WPF project.
    1. import mp3 files
    2. data binding
    3. basic UI

---
+++ Task 3/SoundModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Id3;
+using Microsoft.Win32;
+
+namespace Task_3
+{
+    class SoundModel
+    {
+        public string? Artist { get; set; }
+
+        public string? Album { get; set; }
+
+        public int Track { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Filename { get; set; }
+
+        public SoundModel(string? artist, string? album, int track, string? title, string? filename)
+        {
+            Artist = artist;
+            Album = album;
+            Track = track;
+            Title = title;
+            Filename = filename;
+        }
+
+        static SoundModel[] LoadSoundsFromDirectory(string directory)
+        {
+            string[] mp3Files = Directory.GetFiles(directory, "*.mp3");
+            SoundModel[] soundModels = new SoundModel[mp3Files.Length];
+            int i = 0;
+            foreach (string filename in mp3Files)
+            {
+                using (var mp3 = new Mp3(filename))
+                {
+                    Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
+                    soundModels[i] = new SoundModel(tag.Artists, tag.Album, tag.Track, tag.Title, filename);
+                }
+            }
+
+            return soundModels;
+        }
+
+        public static SoundModel[] LoadSoundsViaDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".mp3";
+            openFileDialog.Filter = "MP3 files (.mp3)|*.mp3";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() != true)
+            {
+                return Array.Empty<SoundModel>();
+            }
+
+            List<SoundModel> smList = new List<SoundModel>();
+            for (int i = 0; i < openFileDialog.FileNames.Length; i++)
+            {
+                string filename = openFileDialog.FileNames[i];
+                string safename = openFileDialog.SafeFileNames[i];
+
+                using (var mp3 = new Mp3(filename))
+                {
+                    Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
+                    smList.Add(new SoundModel(tag.Artists, tag.Album, tag.Track, tag.Title, safename));
+                }
+            }
+
+            return smList.ToArray();
+        }
+    }
+}
---
+++ Task 3/SoundViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Microsoft.Win32;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace Task_3
+{
+    class SoundViewModel : INotifyPropertyChanged
+    {
+        private string? artist;
+        private string? album;
+        private string? track;
+        private string? title;
+        private string? filename;
+
+        public string? Artist
+        {
+            get => artist;
+            private set
+            {
+                artist = value;
+                OnPropertyChanged(nameof(Artist));
+            }
+        }
+
+        public string? Album
+        {
+            get => album;
+            private set
+            {
+                album = value;
+                OnPropertyChanged(nameof(Album));
+            }
+        }
+
+        public string? Track
+        {
+            get => track;
+            private set
+            {
+                track = value;
+                OnPropertyChanged(nameof(Track));
+            }
+        }
+
+        public string? Title
+        {
+            get => title;
+            private set
+            {
+                title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string? Filename
+        {
+            get => filename;
+            private set
+            {
+                filename = value;
+                OnPropertyChanged(nameof(Filename));
+            }
+        }
+
+        public ICommand ImportCommand { get; }
+
+        public class RelayCommand : ICommand
+        {
+            private readonly Action<object?> _execute;
+            private readonly Func<object?, bool>? _canExecute;
+
+            public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+            {
+                _execute = execute;// ?? throw new ArgumentNullException(nameof(execute));
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
+            public void Execute(object? parameter) => _execute(parameter);
+            public event EventHandler? CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+        }
+
+        private ObservableCollection<SoundModel> _sounds = new ObservableCollection<SoundModel>();
+        public ObservableCollection<SoundModel> Sounds
+        {
+            get => _sounds;
+            set
+            {
+                _sounds = value;
+                OnPropertyChanged(nameof(Sounds));
+            }
+        }
+
+        public SoundViewModel()
+        {
+            ImportCommand = new RelayCommand((object? parameter) => 
+            { 
+                Sounds = new ObservableCollection<SoundModel>(SoundModel.LoadSoundsViaDialog()); 
+            });
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
